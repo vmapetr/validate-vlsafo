fc97c7c54c0fcbd8323d543f90e37c3fa28585ba
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-20 09:12:38 +0300

    Update aws-sam-cli.sh

--- aws-sam-cli.sh
+++ aws-sam-cli.sh
@@ -7,7 +7,7 @@ echo "Install aws sam cli"
 
 Python3Url="https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tar.xz"
 mkdir "/opt/python-aws-sam-cli"
-AWSPython3Path="opt/python-aws-sam-cli/python-3.7.8.tar.xz"
+AWSPython3Path="/opt/python-aws-sam-cli/python-3.7.8.tar.xz"
 wget $Python3Url -O $AWSPython3Path
 tar -xzvf $AWSPython3Path -C /opt/python-aws-sam-cli
 
