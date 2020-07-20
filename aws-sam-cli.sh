4b90f1f5bb377a06313edfdc6f8ab5f4cbe72194
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-20 09:37:06 +0300

    Update aws-sam-cli.sh

--- aws-sam-cli.sh
+++ aws-sam-cli.sh
@@ -1,17 +1,19 @@
 echo "Install aws sam cli"
 
-# TarballUrl=$(curl -s https://api.github.com/repos/awslabs/aws-sam-cli/releases/latest | jq -r '.tarball_url')
-# TarballPath="/tmp/aws-sam-cli.tar.gz"
-# wget $TarballUrl -O $TarballPath
-# tar -xzvf $TarballPath -C /tmp
+# Download latest aws sam cli sources
+TarballUrl=$(curl -s https://api.github.com/repos/awslabs/aws-sam-cli/releases/latest | jq -r '.tarball_url')
+TarballPath="/tmp/aws-sam-cli.tar.gz"
+wget $TarballUrl -O $TarballPath
+tar -xzvf $TarballPath -C /tmp
+cd /tmp/awslabs-aws-sam-cli*
 
-Python3Url="https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tar.xz"
-mkdir "/opt/python-aws-sam-cli"
-AWSPython3Path="/opt/python-aws-sam-cli/python-3.7.8.tar.xz"
-wget $Python3Url -O $AWSPython3Path
-tar -xzf $AWSPython3Path -C /opt/python-aws-sam-cli --strip 1
-
+mkdir /opt/python-aws-sam-cli
+cp -r /opt/hostedtoolcache/Python/3.7* /opt/python-aws-sam-cli
 ls /opt/python-aws-sam-cli
 
-# cd /tmp/awslabs-aws-sam-cli*
-
+# Use copy python 3.7.7 from toolcache to install aws sam, setuptools package required for the installation
+# Python3Dir=$(echo /opt/python-aws-sam-cli/Python/3.7*/x64)
+# Python3BinDir="${Python3Dir}/bin"
+# export PATH="$Python3Dir:$Python3BinDir:$PATH"
+# python3 -m pip install setuptools
+# python3 setup.py install
