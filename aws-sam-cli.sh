15e33166ef875d3e9aded4d4510c7cc1ca03d762
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-20 09:11:38 +0300

    Update aws-sam-cli.sh

--- aws-sam-cli.sh
+++ aws-sam-cli.sh
@@ -1,9 +1,9 @@
 echo "Install aws sam cli"
 
-TarballUrl=$(curl -s https://api.github.com/repos/awslabs/aws-sam-cli/releases/latest | jq -r '.tarball_url')
-TarballPath="/tmp/aws-sam-cli.tar.gz"
-wget $TarballUrl -O $TarballPath
-tar -xzvf $TarballPath -C /tmp
+# TarballUrl=$(curl -s https://api.github.com/repos/awslabs/aws-sam-cli/releases/latest | jq -r '.tarball_url')
+# TarballPath="/tmp/aws-sam-cli.tar.gz"
+# wget $TarballUrl -O $TarballPath
+# tar -xzvf $TarballPath -C /tmp
 
 Python3Url="https://www.python.org/ftp/python/3.7.8/Python-3.7.8.tar.xz"
 mkdir "/opt/python-aws-sam-cli"
@@ -13,5 +13,5 @@ tar -xzvf $AWSPython3Path -C /opt/python-aws-sam-cli
 
 ls /opt/python-aws-sam-cli
 
-cd /tmp/awslabs-aws-sam-cli*
+# cd /tmp/awslabs-aws-sam-cli*
 
