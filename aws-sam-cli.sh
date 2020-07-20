0266eb8fde0eb7bdf41917cc9a773b42200612fa
Vladimir Safonkin <vsafonkin@gmail.com>
2020-07-20 13:56:43 +0300

    Update aws-sam-cli.sh

--- aws-sam-cli.sh
+++ aws-sam-cli.sh
@@ -9,8 +9,6 @@ cd /tmp/awslabs-aws-sam-cli*
 
 mkdir /opt/python-aws-sam-cli
 cp -r /opt/hostedtoolcache/Python/3.7.7 /opt/python-aws-sam-cli
-du -hs /opt/python-aws-sam-cli
-echo "-------"
 
 # Use copy python 3.7.7 from toolcache to install aws sam, setuptools package required for the installation
 Python3Dir=$(echo /opt/python-aws-sam-cli/3.7.7/x64)
@@ -18,8 +16,3 @@ Python3BinDir="${Python3Dir}/bin"
 export PATH="$Python3Dir:$Python3BinDir:$PATH"
 python3 -m pip install setuptools
 python3 setup.py install
-
-echo "-------"
-du -hs /opt/python-aws-sam-cli
-echo "AWS sam cli version"
-$Python3BinDir/sam init
