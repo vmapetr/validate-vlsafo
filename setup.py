7a78d8df2873c698dff4e37ebb0bfc336e0ee38c
Vladimir Safonkin <vsafonkin@gmail.com>
2020-08-04 13:22:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,67 @@ extras_require = {
     'xmlio': ['uproot~=3.6'],  # Future proof against uproot4 API changes
     'minuit': ['iminuit~=1.4,>=1.4.3'],  # Use "name" keyword in MINUIT optimizer
 }
+extras_require['backends'] = sorted(
+    set(
+        extras_require['tensorflow']
+        + extras_require['torch']
+        + extras_require['jax']
+        + extras_require['minuit']
+    )
+)
+extras_require['contrib'] = sorted(set(['matplotlib']))
+extras_require['lint'] = sorted(set(['pyflakes', 'black']))
+
+extras_require['test'] = sorted(
+    set(
+        extras_require['backends']
+        + extras_require['xmlio']
+        + extras_require['contrib']
+        + extras_require['shellcomplete']
+        + [
+            'pytest~=3.5',
+            'pytest-cov>=2.5.1',
+            'pytest-mock',
+            'pytest-benchmark[histogram]',
+            'pytest-console-scripts',
+            'pytest-mpl',
+            'pydocstyle',
+            'coverage>=4.0',  # coveralls
+            'papermill~=2.0',
+            'nteract-scrapbook~=0.2',
+            'jupyter',
+            'uproot~=3.3',
+            'graphviz',
+            'jsonpatch',
+        ]
+    )
+)
+extras_require['docs'] = sorted(
+    set(
+        [
+            'sphinx>=3.1.2',
+            'sphinxcontrib-bibtex',
+            'sphinx-click',
+            'sphinx_rtd_theme',
+            'nbsphinx',
+            'ipywidgets',
+            'sphinx-issues',
+            'sphinx-copybutton>0.2.9',
+        ]
+    )
+)
+extras_require['develop'] = sorted(
+    set(
+        extras_require['docs']
+        + extras_require['lint']
+        + extras_require['test']
+        + ['nbdime', 'bumpversion', 'ipython', 'pre-commit', 'check-manifest', 'twine']
+    )
+)
+extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
+
 
 setup(
-    extras_require=extras_require
+    extras_require=extras_require,
+    use_scm_version=lambda: {'local_scheme': lambda version: ''},
 )
